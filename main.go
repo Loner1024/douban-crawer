aa756584104a37b6b378fae06635cc3e7121266c
Loner1024 <xth12138@gmail.com>
2021-02-08 03:46:46 +0800

    extract tag url and tag name

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
 )
 
 const url string = "https://book.douban.com"
@@ -13,7 +14,7 @@ func main()  {
 	client:=&http.Client{
 	
 	}
-	req,err:=http.NewRequest("GET",url+"/tag/",nil)
+	req,err:=http.NewRequest("GET",url+"/tag/程序",nil)
 	req.Header = map[string][]string{
 		"User-Agent": []string{"Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36"},
 		}
@@ -30,5 +31,14 @@ func main()  {
 		return
 	}
 	all,err:=ioutil.ReadAll(resp.Body)
-	fmt.Printf("%s\n",all)
+	// fmt.Printf("%s\n",all)
+	printTagList(all)
 }
+
+
+func printTagList(contents []byte){
+	re:=regexp.MustCompile(`<a href="(/tag/[^>]*)">([^<]*)</a>`)
+	match:=re.FindAllStringSubmatch(string(contents),-1)
+	for _,v :=range match{
+		fmt.Printf("url: %s%s,tag:%s\n",url,v[1],v[2])
+	}}
